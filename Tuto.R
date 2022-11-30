a85125d52450a0cbaf498c97e23b8582da1cfe65
Marie-Caroline Prima <marie-caroline.prima@mnhn.fr>
2022-11-30 09:55:48 +0100

    :space_invader: commit first files

---
+++ Tuto.R
@@ -0,0 +1,7 @@
+# 1. creer un  projet avec git 
+# 2. creer un Rmd et le kniter 
+# 3. utiliser ligne de code ci-dessous pour créer un repo et le pusher
+usethis::use_github(organisation = 'mcpri3')
+# 4. Faire un nouveau commit / push 
+# 5. Sur github ==> settings ==> pages ==> selectionner roots/ main et save ==> le site se crée ; 
+# 6. Si le fichier Rmd initial ne s'appelle pas index alors il faut ajouter le nom du fichier à la fin de l'url /.../file.html par exemple
